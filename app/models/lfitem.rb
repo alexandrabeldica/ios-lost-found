5a56c63b3fe52adcffbaab2df7d8cafca0f1432d
Alexandra Beldica <allex_andrra@yahoo.com>
2013-04-17 23:55:59 +0300

    fixed bug and added validation

--- app/models/lfitem.rb
+++ app/models/lfitem.rb
@@ -4,8 +4,8 @@ class Lfitem < ActiveRecord::Base
 	validates :lftype, :title, :description, :contact, :presence => true
 	belongs_to :category
 	has_one :location, :foreign_key => :lfitem_id, :dependent => :destroy
-	validates_format_of :phone, 
-                    :with => /\A[0-9]{10}\Z/, 
+	validates_format_of :contact, 
+                    :with => /^\+[0-9]{10}/, 
                     :allow_blank => true, 
                     :allow_nil => true
 
